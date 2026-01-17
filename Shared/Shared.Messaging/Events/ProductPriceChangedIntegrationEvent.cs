458d6fed22759b17a9f3aca61b75cf12ad7d61e7
atMou <atmousal@yahoo.com>
2026-01-18 00:03:29 +0100

    chore: remove shared.messaging folder from repository

--- Shared/Shared.Messaging/Events/ProductPriceChangedIntegrationEvent.cs
+++
@@ -1,8 +0,0 @@
-using Shared.Messaging.Abstractions;
-
-namespace Shared.Messaging.Events;
-using System;
-
-public record ProductPriceChangedIntegrationEvent(Guid ProductId, decimal oldPrice, decimal NewPrice) : IntegrationEvent
-{
-}
